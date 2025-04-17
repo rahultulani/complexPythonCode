4c1b5c868fc18b02e435bd92a8667f4771b99015
Rahul Tulani <54772563+rahultulani@users.noreply.github.com>
2025-04-16 23:44:41 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-lambda_ = (lambda f: (lambda x: f(lambda *a: x(x)(*a)))(lambda x: f(lambda *a: x(x)(*a))))
-
-F = lambda f: lambda n: 1 if n < 2 else n * f(n - 1)
-out = lambda_(F)
-
-print(out(6))
+def do_something(n):
+    lambda_ = (lambda f: (lambda x: f(lambda *a: x(x)(*a)))(lambda x: f(lambda *a: x(x)(*a))))
+    
+    F = lambda f: lambda n: 1 if n < 2 else n * f(n - 1)
+    fact = lambda_(F)
+    
+    print(fact(n))
+    
+do_something(6)
